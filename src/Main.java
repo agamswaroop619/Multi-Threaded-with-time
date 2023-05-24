96bcda2d2457fb1692f31757247ccf6f2efd3d1a
agamswaroop619 <agam.swaroop619@gmail.com>
2023-05-24 11:35:34 +0530

    "With join()"

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner in= new Scanner(System.in);
 
         System.out.println("Main Program starts");
